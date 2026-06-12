1c2b677ec354a306e7683cc34996a396bae73f9a
Ponhari-S <ponhari9923@gmail.com>
2026-06-12 18:12:55 +0530

    Tailwind Adv

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,11 +2,11 @@ import MovieCard from "./MovieCard";
 
 const MovieList= ({movie,title}) =>{
     return (
-        <div className="flex flex-col px-10">
-            <h1 className="text-3xl font-bold text-white mb-3 px-2">{title}</h1>
-            <div className="flex flex-row gap-4 overflow-x-auto scrollbar-hide">
+        <div className="px-6 md:px-12 mb-10 relative z-20">
+            <h1 className="text-3l md:text-3xl font-bold text-white">{title}</h1>
+            <div className="flex gap-4 overflow-x-auto scrollbar-hide pb-4 mt-4">
             {movie && movie?.filter(movie => movie.poster_url).map((movies)=>(
-                <div className="flex-shrink-0" key={movies.id}>
+                <div className="flex-shrink-0 transition-transform duration-300 hover:scale-105 hover:z-30 cursor-pointer py-6 px-3" key={movies.id}>
                     <MovieCard movie={movies}/>
                 </div>
             ))}
--- src/components/SecondaryMovie.js
+++ src/components/SecondaryMovie.js
@@ -3,9 +3,12 @@ import MovieList from "./MovieList";
 
 const SecondaryMovie = () =>{
     const movies=useSelector((state)=>state.movie); 
+    if (!movies || movies.length === 0) return null;
     return(
-        <div className="bg-black w-screen flex flex-col gap-8 py-8 -mt-32 relative z-10">
-            <MovieList movie={movies.nowPlaying} title="Now Playing"/>
+        <div className="bg-black w-screen flex flex-col py-8">
+            <div className="-mt-[280px]">
+                <MovieList movie={movies.nowPlaying} title="Now Playing"/>
+            </div>
             <MovieList movie={movies.actionse} title="Action"/>
             <MovieList movie={movies.comedy} title="Comedy"/>
             <MovieList movie={movies.horror} title="Horror"/>
